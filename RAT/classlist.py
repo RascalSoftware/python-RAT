f0ac42f91409cdb6d62d14d41a48f0c22d574659
PaulSharp <44529197+DrPaulSharp@users.noreply.github.com>
2023-08-01 12:15:53 +0100

    Rewrites ClassList to allow the user to specify a name_field, which must have unique values.

--- RAT/classlist.py
+++ RAT/classlist.py
@@ -14,19 +14,22 @@ class ClassList(collections.UserList):
     This class subclasses collections.UserList to construct a list intended to store ONLY instances of a particular
     class, given on initialisation. Any attempt to introduce an object of a different type will raise a ValueError.
     The class must be able to accept attribute values using keyword arguments. In addition, if the class has the
-    attribute "name", the ClassList will ensure that all objects within the ClassList have different names. It is then
-    possible to use the name of an object in the .remove(), .count(), and .index() routines in place of the full
-    object. Due to the requirement of unique names, the multiplication operators __mul__, __rmul__, and __imul__ have
-    been disabled, since they cannot allow for unique names by definition.
+    attribute given in the ClassList's "name_field" attribute (the default is "name"), the ClassList will ensure that
+    all objects within the ClassList have unique values for that attribute. It is then possible to use this attribute
+    of an object in the .remove(), .count(), and .index() routines in place of the full object. Due to the requirement
+    of unique values of the name_field attribute, the multiplication operators __mul__, __rmul__, and __imul__ have
+    been disabled, since they cannot allow for unique attribute values by definition.
 
     We extend the UserList class to enable objects to be added and modified using just the keyword arguments, enable
-    the object name to be used in place of the full object, and ensure all elements are of the specified type, with
-    unique names defined.
+    the object name_field attribute to be used in place of the full object, and ensure all elements are of the
+    specified type, with unique name_field attributes defined.
 
     Parameters
     ----------
     init_list : Sequence [object] or object
         An instance, or list of instance(s), of the class to be used in this ClassList.
+    name_field : str, optional
+        The field used to define unique objects in the ClassList (default is "name").
     empty_list : bool, optional
         If true, do not initialise the list with the contents of init_list, use an empty list instead
         (default is False).
@@ -36,19 +39,23 @@ class ClassList(collections.UserList):
     ValueError
         Raised if the input list is empty.
     """
-
-    def __init__(self, init_list: Union[Sequence[object], object], empty_list: bool = False) -> None:
+    def __init__(self,
+                 init_list: Union[Sequence[object], object],
+                 name_field: str = "name",
+                 empty_list: bool = False) -> None:
         # Set input as list if necessary, raising a ValueError for empty input
         if not (isinstance(init_list, collections.abc.Sequence) and not isinstance(init_list, str)):
             init_list = [init_list]
         if not init_list:
             raise ValueError("Input value must not be empty")
 
-        # Set class to be used for this instance of the ClassList
-        # and check all elements are of the same type and have unique names
+        # Set class to be used for this instance of the ClassList, and the attribute that must contain unique values
         self._class_handle = type(init_list[0])
+        self.name_field = name_field
+
+        # Check all elements are of the same type and have unique values of name_field
         self._check_classes(init_list)
-        self._check_unique_names(init_list)
+        self._check_unique_name_fields(init_list)
 
         super().__init__(init_list)
         if empty_list:
@@ -65,14 +72,14 @@ class ClassList(collections.UserList):
 
     def __setitem__(self, index: int, set_dict: Dict[str, Any]) -> None:
         """Assign the values of an existing object's attributes using a dictionary."""
-        self._validate_name(set_dict)
+        self._validate_name_field(set_dict)
         for key, value in set_dict.items():
             setattr(self.data[index], key, value)
 
     def __iadd__(self, other: Iterable[object]) -> 'ClassList':
         """Define in-place addition using the "+=" operator."""
         self._check_classes(self + other)
-        self._check_unique_names(self + other)
+        self._check_unique_name_fields(self + other)
         super().__iadd__(other)
         return self
 
@@ -90,50 +97,53 @@ class ClassList(collections.UserList):
 
     def append(self, **kwargs) -> None:
         """Append a new object to the ClassList using keyword arguments to set attribute values."""
-        self._validate_name(kwargs)
+        self._validate_name_field(kwargs)
         self.data.append(self._class_handle(**kwargs))
 
     def insert(self, index: int, **kwargs) -> None:
         """Insert a new object into the ClassList at a given index using keyword arguments to set attribute values."""
-        self._validate_name(kwargs)
+        self._validate_name_field(kwargs)
         self.data.insert(index, self._class_handle(**kwargs))
 
     def remove(self, item: Union[object, str]) -> None:
-        """Remove an object from the ClassList using either the object itself or its name."""
+        """Remove an object from the ClassList using either the object itself or its name_field value."""
         if not isinstance(item, self._class_handle):
-            item = self._get_item_from_name(item)
+            item = self._get_item_from_name_field(item)
         self.data.remove(item)
 
     def count(self, item: Union[object, str]) -> int:
-        """Return the number of times an object appears in the ClassList using either the object itself or its name."""
+        """Return the number of times an object appears in the ClassList using either the object itself or its
+        name_field value."""
         if not isinstance(item, self._class_handle):
-            item = self._get_item_from_name(item)
+            item = self._get_item_from_name_field(item)
         return self.data.count(item)
 
     def index(self, item: Union[object, str], *args) -> int:
-        """Return the index of a particular object in the ClassList using either the object itself or its name."""
+        """Return the index of a particular object in the ClassList using either the object itself or its
+        name_field value."""
         if not isinstance(item, self._class_handle):
-            item = self._get_item_from_name(item)
+            item = self._get_item_from_name_field(item)
         return self.data.index(item, *args)
 
     def extend(self, other: Iterable[object]) -> None:
         """Extend the ClassList by adding another iterable."""
         self._check_classes(self + other)
-        self._check_unique_names(self + other)
+        self._check_unique_name_fields(self + other)
         self.data.extend(other)
 
     def get_names(self) -> list[str]:
-        """Return a list of the names of each class object in the list.
+        """Return a list of the values of the name_field attribute of each class object in the list.
 
         Returns
         -------
         names : list [str]
-            The value of the name attribute of each object in the ClassList.
+            The value of the name_field attribute of each object in the ClassList.
         """
-        return [model.name for model in self.data if hasattr(model, 'name')]
+        return [getattr(model, self.name_field) for model in self.data if hasattr(model, self.name_field)]
 
-    def _validate_name(self, input_args: Dict[str, Any]) -> None:
-        """Raise a ValueError if a name is passed as an object parameter, and it is already used within the ClassList.
+    def _validate_name_field(self, input_args: Dict[str, Any]) -> None:
+        """Raise a ValueError if the name_field attribute is passed as an object parameter, and its value is already
+        used within the ClassList.
 
         Parameters
         ----------
@@ -143,17 +153,17 @@ class ClassList(collections.UserList):
         Raises
         ------
         ValueError
-            Raised if the input arguments contain a name already defined in the ClassList.
+            Raised if the input arguments contain a name_field value already defined in the ClassList.
         """
         names = self.get_names()
         with contextlib.suppress(KeyError):
-            if input_args['name'] in names:
-                raise ValueError(f"Input arguments contain the name '{input_args['name']}',"
-                                 f" which is already specified in the ClassList")
+            if input_args[self.name_field] in names:
+                raise ValueError(f"Input arguments contain the {self.name_field} '{input_args[self.name_field]}', "
+                                 f"which is already specified in the ClassList")
 
-    @staticmethod
-    def _check_unique_names(input_list: Iterable) -> None:
-        """Raise a ValueError if any value of the "name" attribute is used more than once in a list of class objects.
+    def _check_unique_name_fields(self, input_list: Iterable) -> None:
+        """Raise a ValueError if any value of the name_field attribute is used more than once in a list of class
+        objects.
 
         Parameters
         ----------
@@ -163,11 +173,11 @@ class ClassList(collections.UserList):
         Raises
         ------
         ValueError
-            Raised if the input list defines more than one object with the same name.
+            Raised if the input list defines more than one object with the same value of name_field.
         """
-        names = [model.name for model in input_list if hasattr(model, 'name')]
+        names = [getattr(model, self.name_field) for model in input_list if hasattr(model, self.name_field)]
         if len(set(names)) != len(names):
-            raise ValueError("Input list contains objects with the same name")
+            raise ValueError(f"Input list contains objects with the same value of the {self.name_field} attribute")
 
     def _check_classes(self, input_list: Iterable[object]) -> None:
         """Raise a ValueError if any object in a list of objects is not of the type specified by self._class_handle.
@@ -185,18 +195,18 @@ class ClassList(collections.UserList):
         if not (all(isinstance(element, self._class_handle) for element in input_list)):
             raise ValueError(f"Input list contains elements of type other than '{self._class_handle}'")
 
-    def _get_item_from_name(self, name: str) -> Union[object, str]:
-        """Return the object with the given value of the attribute "name" in the ClassList.
+    def _get_item_from_name_field(self, value: str) -> Union[object, str]:
+        """Return the object with the given value of the name_field attribute in the ClassList.
 
         Parameters
         ----------
-        name : str
-            The name of an object in the ClassList.
+        value : str
+            The value of the name_field attribute of an object in the ClassList.
 
         Returns
         -------
         instance : object or str
-            Either the object with the value of the "name" attribute given by name, or the input name if an object with
-            that value of the "name" attribute cannot be found.
+            Either the object with the value of the name_field attribute given by value, or the input value if an
+            object with that value of the name_field attribute cannot be found.
         """
-        return next((model for model in self.data if model.name == name), name)
+        return next((model for model in self.data if getattr(model, self.name_field) == value), value)
--- tests/test_classlist.py
+++ tests/test_classlist.py
@@ -83,10 +83,10 @@ class TestInitialisation(object):
     @pytest.mark.parametrize("input_list", [
         ([DotDict(name='Alice'), DotDict(name='Alice')]),
     ])
-    def test_identical_names(self, input_list: Sequence) -> None:
-        """If we initialise a ClassList with input objects with identical "name" fields,
+    def test_identical_name_fields(self, input_list: Sequence) -> None:
+        """If we initialise a ClassList with input objects with identical values of the name_field,
         we should raise a ValueError."""
-        with pytest.raises(ValueError, match="Input list contains objects with the same name"):
+        with pytest.raises(ValueError, match=f"Input list contains objects with the same value of the name attribute"):
             ClassList(input_list)
 
 
@@ -123,9 +123,10 @@ def test_setitem(two_name_class_list: 'ClassList', new_values: Dict, expected_cl
 @pytest.mark.parametrize("new_values", [
     ({'name': 'Bob'}),
 ])
-def test_setitem_same_name(two_name_class_list: 'ClassList', new_values: Dict) -> None:
-    """If we set the name of an object in the ClassList to one already defined, we should raise a ValueError."""
-    with pytest.raises(ValueError, match=f"Input arguments contain the name '{new_values['name']}', "
+def test_setitem_same_name_field(two_name_class_list: 'ClassList', new_values: Dict) -> None:
+    """If we set the name_field of an object in the ClassList to one already defined, we should raise a ValueError."""
+    with pytest.raises(ValueError, match=f"Input arguments contain the {two_name_class_list.name_field} "
+                                         f"'{new_values[two_name_class_list.name_field]}', "
                                          f"which is already specified in the ClassList"):
         two_name_class_list[0] = new_values
 
@@ -182,9 +183,10 @@ def test_append(two_name_class_list: 'ClassList', new_values: Dict, three_name_c
 @pytest.mark.parametrize("new_values", [
     ({'name': 'Alice'}),
 ])
-def test_append_same_name(two_name_class_list: 'ClassList', new_values: Dict) -> None:
-    """If we append an object with an already-specified name to a ClassList we should raise a ValueError."""
-    with pytest.raises(ValueError, match=f"Input arguments contain the name '{new_values['name']}', "
+def test_append_same_name_field(two_name_class_list: 'ClassList', new_values: Dict) -> None:
+    """If we append an object with an already-specified name_field value to a ClassList we should raise a ValueError."""
+    with pytest.raises(ValueError, match=f"Input arguments contain the {two_name_class_list.name_field} "
+                                         f"'{new_values[two_name_class_list.name_field]}', "
                                          f"which is already specified in the ClassList"):
         two_name_class_list.append(**new_values)
 
@@ -202,9 +204,10 @@ def test_insert(two_name_class_list: 'ClassList', new_values: Dict) -> None:
     ({'name': 'Alice'})
 ])
 def test_insert_same_name(two_name_class_list: 'ClassList', new_values: Dict) -> None:
-    """If we insert an object with an already-specified name to a ClassList we should raise a ValueError."""
-    with pytest.raises(ValueError, match=(f"Input arguments contain the name '{new_values['name']}',"
-                                          f" which is already specified in the ClassList")):
+    """If we insert an object with an already-specified name_field value to a ClassList we should raise a ValueError."""
+    with pytest.raises(ValueError, match=f"Input arguments contain the {two_name_class_list.name_field} "
+                                         f"'{new_values[two_name_class_list.name_field]}', "
+                                         f"which is already specified in the ClassList"):
         two_name_class_list.insert(1, **new_values)
 
 
@@ -213,7 +216,8 @@ def test_insert_same_name(two_name_class_list: 'ClassList', new_values: Dict) ->
     (DotDict(name='Bob')),
 ])
 def test_remove(two_name_class_list: 'ClassList', remove_value: Union[object, str]) -> None:
-    """We should be able to remove an object either by name or by specifying the object itself."""
+    """We should be able to remove an object either by the value of the name_field or by specifying the object
+    itself."""
     two_name_class_list.remove(remove_value)
     assert two_name_class_list == ClassList([DotDict(name='Alice')])
 
@@ -236,7 +240,7 @@ def test_remove_not_present(two_name_class_list: 'ClassList', remove_value: Unio
 ])
 def test_count(two_name_class_list: 'ClassList', count_value: Union[object, str], expected_count: int) -> None:
     """We should be able to determine the number of times an object is in the ClassList using either the object itself
-    or its name.
+    or its name_field value.
     """
     assert two_name_class_list.count(count_value) == expected_count
 
@@ -246,8 +250,8 @@ def test_count(two_name_class_list: 'ClassList', count_value: Union[object, str]
     (DotDict(name='Bob'), 1),
 ])
 def test_index(two_name_class_list: 'ClassList', index_value: Union[object, str], expected_index: int) -> None:
-    """We should be able to find the index of an object in the ClassList either by name or by specifying the object
-    itself.
+    """We should be able to find the index of an object in the ClassList either by its name_field value or by
+    specifying the object itself.
     """
     assert two_name_class_list.index(index_value) == expected_index
 
@@ -282,7 +286,8 @@ def test_extend(two_name_class_list: 'ClassList', extended_list: Iterable, three
     (ClassList(DotDict(), empty_list=True), []),
 ])
 def test_get_names(class_list: 'ClassList', expected_names: List[str]) -> None:
-    """We should get a list of the values of the "name" attribute from each object with it defined in the ClassList."""
+    """We should get a list of the values of the name_field attribute from each object with it defined in the
+    ClassList."""
     assert class_list.get_names() == expected_names
 
 
@@ -290,19 +295,21 @@ def test_get_names(class_list: 'ClassList', expected_names: List[str]) -> None:
     ({'name': 'Eve'}),
     ({'surname': 'Polastri'}),
 ])
-def test__validate_name(two_name_class_list: 'ClassList', input_dict: Dict) -> None:
-    """We should not raise an error if the input values do not contain a name defined in an object in the ClassList."""
-    assert two_name_class_list._validate_name(input_dict) is None
+def test__validate_name_field(two_name_class_list: 'ClassList', input_dict: Dict) -> None:
+    """We should not raise an error if the input values do not contain a name_field value defined in an object in the
+    ClassList."""
+    assert two_name_class_list._validate_name_field(input_dict) is None
 
 
 @pytest.mark.parametrize("input_dict", [
     ({'name': 'Alice'}),
 ])
-def test__validate_name_not_unique(two_name_class_list: 'ClassList', input_dict: Dict) -> None:
-    """We should raise a ValueError if we input values containing a name defined in an object in the ClassList."""
-    with pytest.raises(ValueError, match=(f"Input arguments contain the name '{input_dict['name']}',"
-                                          f" which is already specified in the ClassList")):
-        two_name_class_list._validate_name(input_dict)
+def test__validate_name_field_not_unique(two_name_class_list: 'ClassList', input_dict: Dict) -> None:
+    """We should raise a ValueError if we input values containing a name_field defined in an object in the ClassList."""
+    with pytest.raises(ValueError, match=f"Input arguments contain the {two_name_class_list.name_field} "
+                                         f"'{input_dict[two_name_class_list.name_field]}', "
+                                         f"which is already specified in the ClassList"):
+        two_name_class_list._validate_name_field(input_dict)
 
 
 @pytest.mark.parametrize("input_list", [
@@ -312,18 +319,21 @@ def test__validate_name_not_unique(two_name_class_list: 'ClassList', input_dict:
     ([DotDict()]),
     ([]),
 ])
-def test__check_unique_names(input_list: Iterable) -> None:
-    """We should not raise an error if an input list contains objects with different names, or no names defined."""
-    assert ClassList._check_unique_names(input_list) is None
+def test__check_unique_name_fields(two_name_class_list: 'ClassList', input_list: Iterable) -> None:
+    """We should not raise an error if an input list contains objects with different name_field values, or if the
+    name_field is not defined."""
+    assert two_name_class_list._check_unique_name_fields(input_list) is None
 
 
 @pytest.mark.parametrize("input_list", [
     ([DotDict(name='Alice'), DotDict(name='Alice')]),
 ])
-def test__check_unique_names_not_unique(input_list: Iterable) -> None:
-    """We should raise a ValueError if an input list contains multiple object with matching names defined."""
-    with pytest.raises(ValueError, match="Input list contains objects with the same name"):
-        ClassList._check_unique_names(input_list)
+def test__check_unique_name_fields_not_unique(two_name_class_list: 'ClassList', input_list: Iterable) -> None:
+    """We should raise a ValueError if an input list contains multiple objects with matching name_field values
+    defined."""
+    with pytest.raises(ValueError, match=f"Input list contains objects with the same value of the "
+                                         f"{two_name_class_list.name_field} attribute"):
+        two_name_class_list._check_unique_name_fields(input_list)
 
 
 @pytest.mark.parametrize("input_list", [
@@ -346,12 +356,14 @@ def test__check_classes_different_classes(input_list: Iterable) -> None:
         class_list._check_classes(input_list)
 
 
-@pytest.mark.parametrize(["name", "expected_output"], [
+@pytest.mark.parametrize(["value", "expected_output"], [
     ("Alice", DotDict(name='Alice')),
     ("Eve", "Eve"),
 ])
-def test__get_item_from_name(two_name_class_list: 'ClassList', name: str, expected_output: Union[object, str]) -> None:
-    """When we input the name of an object defined in the ClassList, we should return the object. If the name is not
-    the name of an object defined in the ClassList, we should return the name.
+def test__get_item_from_name_field(two_name_class_list: 'ClassList',
+                                   value: str,
+                                   expected_output: Union[object, str]) -> None:
+    """When we input the name_field value of an object defined in the ClassList, we should return the object.
+    If the value is not the name_field of an object defined in the ClassList, we should return the value.
     """
-    assert two_name_class_list._get_item_from_name(name) == expected_output
+    assert two_name_class_list._get_item_from_name_field(value) == expected_output
