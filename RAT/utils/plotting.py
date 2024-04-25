87d65717eea353ddccfbe32e18d49d7e1ce9dee3
RabiyaF <47083562+RabiyaF@users.noreply.github.com>
2024-04-25 16:32:29 +0100

    Added the figure class

--- RAT/utils/plotting.py
+++ RAT/utils/plotting.py
@@ -6,160 +6,31 @@ import numpy as np
 from RAT.rat_core import PlotEventData, makeSLDProfileXY
 
 
-class RATPlots:
+class Figure:
     """
-    Creates the RAT reflectivity and Scattering
-    Lenght Density (SLD) plots
+    Creates a plotting figure.
     """
 
-    def __init__(self, delay: bool = True):
+    def __init__(self, row: int = 1, col: int = 2):
         """
-        Initializes the figure and the subplots
+        Initializes the figure and the subplots.
+
+        Parameters
+        ----------
+        row : int
+              The number of rows in subplot
+        col : int
+              The number of columns in subplot
         """
-        self._fig, (self._ref_plot, self._sld_plot) = \
-            plt.subplots(1, 2, num="Reflectivity Algorithms Toolbox (RAT)")
+        self._fig, self._ax = \
+            plt.subplots(row, col, num="Reflectivity Algorithms Toolbox (RAT)")
         plt.show(block=False)
-        self._delay = delay
         self._esc_pressed = False
         self._close_clicked = False
         self._fig.canvas.mpl_connect("key_press_event",
                                      self._process_button_press)
         self._fig.canvas.mpl_connect('close_event',
                                      self._close)
-
-    def _plot(self, data: PlotEventData):
-        """
-        Clears the previous plots and updates the ref and SLD plots.
-
-        Parameters
-        ----------
-        data : PlotEventData
-            The plot event data that contains all the information
-            to generate the ref and sld plots
-        """
-        # Clears the previous plots
-        self._ref_plot.cla()
-        self._sld_plot.cla()
-
-        for i, (r, sd, sld, layer) in enumerate(zip(data.reflectivity,
-                                                    data.shiftedData,
-                                                    data.sldProfiles,
-                                                    data.resampledLayers)):
-
-            r, sd, sld, layer = map(lambda x: x[0], (r, sd, sld, layer))
-
-            # Calculate the divisor
-            div = 1 if i == 0 else 2**(4*(i+1))
-
-            # Plot the reflectivity on plot (1,1)
-            self._ref_plot.plot(r[:, 0],
-                                r[:, 1]/div,
-                                label=f'ref {i+1}',
-                                linewidth=2)
-            self.color = self._ref_plot.get_lines()[-1].get_color()
-            if data.dataPresent[i]:
-                self._plot_shifted_data(sd, div)
-
-            # Plot the slds on plot (1,2)
-            for j in range(1, sld.shape[1]):
-                self._sld_plot.plot(sld[:, 0],
-                                    sld[:, j],
-                                    label=f'sld {i+1}',
-                                    color=self.color,
-                                    linewidth=2)
-
-            if data.resample[i] == 1 or data.modelType == 'custom xy':
-                new = makeSLDProfileXY(layer[0, 1],
-                                       layer[-1, 1],
-                                       data.subRoughs[i],
-                                       layer,
-                                       len(layer),
-                                       1.0)
-
-                self._sld_plot.plot([row[0]-49 for row in new],
-                                    [row[1] for row in new],
-                                    color=self.color,
-                                    linewidth=1)
-
-        self._format_plots()
-
-        if self._delay:
-            plt.pause(0.005)
-
-    def _plot_shifted_data(self, sd, div):
-        """
-        Plots the shifted data.
-
-        Parameters
-        ----------
-        sd : np.ndarray
-            The shifted data containing the x, y, e data
-        div : int
-            The divisor for the data
-        """
-        sd_x = sd[:, 0]
-        sd_y, sd_e = map(lambda x: x/div, (sd[:, 1], sd[:, 2]))
-
-        # Plot the errorbars
-        indices_removed = np.flip(np.nonzero(sd_y - sd_e < 0)[0])
-        sd_x_r, sd_y_r, sd_e_r = map(lambda x:
-                                     np.delete(x, indices_removed),
-                                     (sd_x, sd_y, sd_e))
-        self._plot_errorbars(sd_x_r, sd_y_r, sd_e_r, False)
-
-        # Plot one sided errorbars
-        indices_selected = [x for x in indices_removed
-                            if x not in np.nonzero(sd_y < 0)[0]]
-        sd_x_s, sd_y_s, sd_e_s = map(lambda x:
-                                     [x[i] for i in indices_selected],
-                                     (sd_x, sd_y, sd_e))
-        self._plot_errorbars(sd_x_s, sd_y_s, sd_e_s, True)
-
-    def _plot_errorbars(self, x, y, err, onesided):
-        """
-        Plots the error bars.
-
-        Parameters
-        ----------
-        x : np.ndarray
-            The shifted data x axis data
-        y : np.ndarray
-            The shifted data y axis data
-        err : np.ndarray
-              The shifted data e data
-        onesided : bool
-            A boolean to indicate whether
-            to draw one sided errorbars
-        """
-        y_error = [[0]*len(err), err] if onesided else err
-        self._ref_plot.errorbar(x=x,
-                                y=y,
-                                yerr=y_error,
-                                fmt='none',
-                                ecolor=self.color,
-                                elinewidth=1,
-                                capsize=0)
-        self._ref_plot.scatter(x=x,
-                               y=y,
-                               s=3,
-                               marker="o",
-                               color=self.color)
-
-    def _format_plots(self):
-        """
-        Formats the ref and sld subplots.
-        """
-        self._ref_plot.set_yscale('log')
-        self._ref_plot.set_xscale('log')
-        self._ref_plot.set_xlabel('Qz')
-        self._ref_plot.set_ylabel('Ref')
-        self._ref_plot.legend()
-        self._ref_plot.grid()
-
-        self._sld_plot.set_xlabel('Z')
-        self._sld_plot.set_ylabel('SLD')
-        self._sld_plot.legend()
-        self._sld_plot.grid()
 
     def wait_for_close(self):
         """
@@ -182,3 +53,131 @@ class RATPlots:
         Process the close_event.
         """
         self._close_clicked = True
+
+
+def plot_errorbars(ax, x, y, err, onesided, color):
+    """
+    Plots the error bars.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes._axes.Axes
+            The axis on which to draw errorbars
+    x : np.ndarray
+        The shifted data x axis data
+    y : np.ndarray
+        The shifted data y axis data
+    err : np.ndarray
+            The shifted data e data
+    onesided : bool
+        A boolean to indicate whether to draw one sided errorbars
+    color : str
+            The hex representing the color of the errorbars
+    """
+    y_error = [[0]*len(err), err] if onesided else err
+    ax.errorbar(x=x,
+                y=y,
+                yerr=y_error,
+                fmt='none',
+                ecolor=color,
+                elinewidth=1,
+                capsize=0)
+    ax.scatter(x=x, y=y, s=3, marker="o", color=color)
+
+
+def plot_ref_sld(data: PlotEventData, fig: Figure = None, delay: bool = True):
+    """
+    Clears the previous plots and updates the ref and SLD plots.
+
+    Parameters
+    ----------
+    data : PlotEventData
+           The plot event data that contains all the information
+           to generate the ref and sld plots
+    fig : Figure
+          The figure class that has two subplots
+    delay : bool
+            Controls whether to delay 0.005s after plot is created
+    """
+    if fig is None:
+        fig = Figure()
+
+    ref_plot = fig._ax[0]
+    sld_plot = fig._ax[1]
+
+    # Clears the previous plots
+    ref_plot.cla()
+    sld_plot.cla()
+
+    for i, (r, sd, sld, layer) in enumerate(zip(data.reflectivity,
+                                                data.shiftedData,
+                                                data.sldProfiles,
+                                                data.resampledLayers)):
+
+        r, sd, sld, layer = map(lambda x: x[0], (r, sd, sld, layer))
+
+        # Calculate the divisor
+        div = 1 if i == 0 else 2**(4*(i+1))
+
+        # Plot the reflectivity on plot (1,1)
+        ref_plot.plot(r[:, 0],
+                      r[:, 1]/div,
+                      label=f'ref {i+1}',
+                      linewidth=2)
+        color = ref_plot.get_lines()[-1].get_color()
+
+        if data.dataPresent[i]:
+            sd_x = sd[:, 0]
+            sd_y, sd_e = map(lambda x: x/div, (sd[:, 1], sd[:, 2]))
+
+            # Plot the errorbars
+            indices_removed = np.flip(np.nonzero(sd_y - sd_e < 0)[0])
+            sd_x_r, sd_y_r, sd_e_r = map(lambda x:
+                                         np.delete(x, indices_removed),
+                                         (sd_x, sd_y, sd_e))
+            plot_errorbars(ref_plot, sd_x_r, sd_y_r, sd_e_r, False, color)
+
+            # Plot one sided errorbars
+            indices_selected = [x for x in indices_removed
+                                if x not in np.nonzero(sd_y < 0)[0]]
+            sd_x_s, sd_y_s, sd_e_s = map(lambda x:
+                                         [x[i] for i in indices_selected],
+                                         (sd_x, sd_y, sd_e))
+            plot_errorbars(ref_plot, sd_x_s, sd_y_s, sd_e_s, True, color)
+
+        # Plot the slds on plot (1,2)
+        for j in range(1, sld.shape[1]):
+            sld_plot.plot(sld[:, 0],
+                          sld[:, j],
+                          label=f'sld {i+1}',
+                          color=color,
+                          linewidth=2)
+
+        if data.resample[i] == 1 or data.modelType == 'custom xy':
+            new = makeSLDProfileXY(layer[0, 1],
+                                   layer[-1, 1],
+                                   data.subRoughs[i],
+                                   layer,
+                                   len(layer),
+                                   1.0)
+
+            sld_plot.plot([row[0]-49 for row in new],
+                          [row[1] for row in new],
+                          color=color,
+                          linewidth=1)
+
+    # Format the axis
+    ref_plot.set_yscale('log')
+    ref_plot.set_xscale('log')
+    ref_plot.set_xlabel('Qz')
+    ref_plot.set_ylabel('Ref')
+    ref_plot.legend()
+    ref_plot.grid()
+
+    sld_plot.set_xlabel('Z')
+    sld_plot.set_ylabel('SLD')
+    sld_plot.legend()
+    sld_plot.grid()
+
+    if delay:
+        plt.pause(0.005)
--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -8,7 +8,7 @@ from unittest.mock import MagicMock
 import numpy as np
 import matplotlib.pyplot as plt
 from RAT.rat_core import PlotEventData
-from RAT.utils.plotting import RATPlots
+from RAT.utils.plotting import Figure, plot_ref_sld
 
 
 TEST_DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
@@ -36,57 +36,62 @@ def data() -> PlotEventData:
 
 
 @pytest.fixture
-def rat_plots() -> RATPlots:
+def fig() -> Figure:
     """
     Creates the fixture for the tests.
     """
-    rat_plots = RATPlots()
-    rat_plots._plot(data())
-    return rat_plots
+    fig = Figure(row=1, col=2)
+    plot_ref_sld(data(), fig)
+    return fig
 
 
-def test_figure_axis_formating(rat_plots: RATPlots) -> None:
+def test_figure_axis_formating(fig: Figure) -> None:
     """
     Tests the axis formating of the figure.
     """
-    assert rat_plots._fig.axes[0].get_subplotspec().get_gridspec().get_geometry() == (1, 2)
+    ref_plot = fig._ax[0]
+    sld_plot = fig._ax[1]
 
-    assert rat_plots._ref_plot.get_xlabel() == "Qz"
-    assert rat_plots._ref_plot.get_xscale() == "log"
-    assert rat_plots._ref_plot.get_ylabel() == "Ref"
-    assert rat_plots._ref_plot.get_yscale() == "log"
-    assert [label._text for label in rat_plots._ref_plot.get_legend().texts] == ['ref 1', 'ref 2', 'ref 3']
+    assert fig._fig.axes[0].get_subplotspec().get_gridspec().get_geometry() == (1, 2)
 
-    assert rat_plots._sld_plot.get_xlabel() == "Z"
-    assert rat_plots._sld_plot.get_xscale() == "linear"
-    assert rat_plots._sld_plot.get_ylabel() == "SLD"
-    assert rat_plots._sld_plot.get_yscale() == "linear"
-    assert [label._text for label in rat_plots._sld_plot.get_legend().texts] == ['sld 1', 'sld 2', 'sld 3']
+    assert ref_plot.get_xlabel() == "Qz"
+    assert ref_plot.get_xscale() == "log"
+    assert ref_plot.get_ylabel() == "Ref"
+    assert ref_plot.get_yscale() == "log"
+    assert [label._text for label in ref_plot.get_legend().texts] == ['ref 1', 'ref 2', 'ref 3']
+
+    assert sld_plot.get_xlabel() == "Z"
+    assert sld_plot.get_xscale() == "linear"
+    assert sld_plot.get_ylabel() == "SLD"
+    assert sld_plot.get_yscale() == "linear"
+    assert [label._text for label in sld_plot.get_legend().texts] == ['sld 1', 'sld 2', 'sld 3']
 
 
-def test_figure_color_formating(rat_plots: RATPlots) -> None:
+def test_figure_color_formating(fig: Figure) -> None:
     """
     Tests the color formating of the figure.
     """
-    ref_plot_line_lenght = len(rat_plots._ref_plot.get_lines())
-    assert ref_plot_line_lenght == 3
-    assert len(rat_plots._sld_plot.get_lines()) == 6
+    ref_plot = fig._ax[0]
+    sld_plot = fig._ax[1]
 
-    for axis_ix in range(ref_plot_line_lenght):
+    assert len(ref_plot.get_lines()) == 3
+    assert len(sld_plot.get_lines()) == 6
+
+    for axis_ix in range(len(ref_plot.get_lines())):
         ax1 = axis_ix*2
         ax2 = ax1 + 1
 
         # Tests whether the color of the line and the errorbars match on the ref_plot
-        assert (rat_plots._ref_plot.containers[ax1][2][0]._original_edgecolor ==
-                rat_plots._ref_plot.containers[ax2][2][0]._original_edgecolor ==
-                rat_plots._ref_plot.get_lines()[axis_ix].get_color())
+        assert (ref_plot.containers[ax1][2][0]._original_edgecolor ==
+                ref_plot.containers[ax2][2][0]._original_edgecolor ==
+                ref_plot.get_lines()[axis_ix].get_color())
 
         # Tests whether the color of the sld and resampled_sld match on the sld_plot
-        assert (rat_plots._sld_plot.get_lines()[ax1].get_color() ==
-                rat_plots._sld_plot.get_lines()[ax2].get_color())
+        assert (sld_plot.get_lines()[ax1].get_color() ==
+                sld_plot.get_lines()[ax2].get_color())
 
 
-def test_eventhandlers_linked_to_figure(rat_plots: RATPlots) -> None:
+def test_eventhandlers_linked_to_figure(fig: Figure) -> None:
     """
     Tests whether the eventhandlers for close_event
     and key_press_event in the figure are linked to the
@@ -94,53 +99,53 @@ def test_eventhandlers_linked_to_figure(rat_plots: RATPlots) -> None:
     """
     pattern = r'\(([^\]]+)\)'
 
-    for ix, val in rat_plots._fig.canvas.callbacks.callbacks['close_event'].items():
+    for ix, val in fig._fig.canvas.callbacks.callbacks['close_event'].items():
         if str(type(val)) == "<class 'weakref.WeakMethod'>":
             break
-    canvas_close_event_callback = rat_plots._fig.canvas.callbacks.callbacks['close_event'][ix]._func_ref.__repr__()
+    canvas_close_event_callback = fig._fig.canvas.callbacks.callbacks['close_event'][ix]._func_ref.__repr__()
     close_event_callback = re.findall(pattern,
                                       canvas_close_event_callback)[0]
     assert close_event_callback == "_close"
-    assert hasattr(RATPlots, "_close")
+    assert hasattr(Figure, "_close")
 
-    for ix, val in rat_plots._fig.canvas.callbacks.callbacks['key_press_event'].items():
+    for ix, val in fig._fig.canvas.callbacks.callbacks['key_press_event'].items():
         if str(type(val)) == "<class 'weakref.WeakMethod'>":
             break
-    canvas_key_press_event_callback = rat_plots._fig.canvas.callbacks.callbacks['key_press_event'][ix]._func_ref.__repr__()
+    canvas_key_press_event_callback = fig._fig.canvas.callbacks.callbacks['key_press_event'][ix]._func_ref.__repr__()
     key_press_event_callback = re.findall(pattern,
                                           canvas_key_press_event_callback)[0]
     assert key_press_event_callback == "_process_button_press"
-    assert hasattr(RATPlots, "_process_button_press")
+    assert hasattr(Figure, "_process_button_press")
 
-def test_eventhandler_variable_update(rat_plots: RATPlots) -> None:
+def test_eventhandler_variable_update(fig: Figure) -> None:
     """
     Tests whether the eventhandlers for close_event
     and key_press_event update variables that stop
     while loop in wait_for_close.
     """
-    assert not rat_plots._esc_pressed
+    assert not fig._esc_pressed
     on_key_mock_event = type('MockEvent', (object,), {'key': 'escape'})
-    rat_plots._process_button_press(on_key_mock_event)
-    assert rat_plots._esc_pressed
+    fig._process_button_press(on_key_mock_event)
+    assert fig._esc_pressed
 
-    assert not rat_plots._close_clicked
-    plt.close(rat_plots._fig)
-    assert rat_plots._close_clicked
+    assert not fig._close_clicked
+    plt.close(fig._fig)
+    assert fig._close_clicked
 
 
 @patch("RAT.utils.plotting.plt.waitforbuttonpress")  
-def test_wait_for_close(mock: MagicMock, rat_plots: RATPlots) -> None:
+def test_wait_for_close(mock: MagicMock, fig: Figure) -> None:
     """
     Tests the _wait_for_close method stops the
     while loop when _esc_pressed is True.
     """
     def mock_wait_for_button_press(timeout):
-        rat_plots._esc_pressed = True
+        fig._esc_pressed = True
 
     mock.side_effect = mock_wait_for_button_press
-    assert not rat_plots._esc_pressed
-    rat_plots.wait_for_close()
-    assert rat_plots._esc_pressed
+    assert not fig._esc_pressed
+    fig.wait_for_close()
+    assert fig._esc_pressed
 
 
 @patch("RAT.utils.plotting.makeSLDProfileXY")   
@@ -149,8 +154,8 @@ def test_sld_profile_function_call(mock: MagicMock) -> None:
     Tests the makeSLDProfileXY function called with
     correct args.
     """
-    rat_plots = RATPlots()
-    rat_plots._plot(data())
+    fig = Figure(row=1, col=2)
+    plot_ref_sld(data(), fig)
 
     assert mock.call_count == 3
     assert mock.call_args_list[0].args[0] == 2.07e-06
