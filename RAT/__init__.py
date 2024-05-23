e6c7dfac8fa20d9f62af5b78f2b24907c5fd66dc
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2024-05-23 15:40:29 +0100

    Adds file "run.py"

--- RAT/__init__.py
+++ RAT/__init__.py
@@ -2,6 +2,7 @@ import os
 from RAT.classlist import ClassList
 from RAT.project import Project
 from RAT.controls import set_controls
+from RAT.run import run
 import RAT.models
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
