040349db9aa69621fb3d9bf225dd60fd7d038933
alexhroom <alex.room@btinternet.com>
2024-08-05 08:10:48 +0100

    changed to field validator

--- RATapi/models.py
+++ RATapi/models.py
@@ -62,16 +62,17 @@ class Background(RATModel):
     value_4: str = ""
     value_5: str = ""
 
-    @model_validator(mode="after")
-    def validate_unimplemented_backgrounds(self):
+    @field_validator("type")
+    @classmethod
+    def validate_unimplemented_backgrounds(cls, type):
         """Raise an error if currently unsupported Data or Function backgrounds are used."""
         # FIXME: once data/function backgrounds have been implemented,
         # please remember to remove the @pytest.mark.skip decorators used to skip the tests:
         # - tests/test_project.py::test_check_allowed_background_resolution_values_data
         # - tests/test_project.py::test_check_allowed_background_resolution_values_on_data_list
-        if self.type == TypeOptions.Data:
+        if type == TypeOptions.Data:
             raise NotImplementedError("Data backgrounds are not yet supported.")
-        elif self.type == TypeOptions.Function:
+        elif type == TypeOptions.Function:
             raise NotImplementedError("Function backgrounds are not yet supported.")
 
 
