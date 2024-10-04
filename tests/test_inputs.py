08c10a36a5ef516864c1634e90e7735ff4abf092
Stephen Nneji <steve.nneji@gmail.com>
2024-10-04 11:37:17 +0100

    Addresses review comments

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -829,30 +829,27 @@ def check_cells_equal(actual_cells, expected_cells) -> None:
         "NaN" if np.isnan(el) else el for entry in actual_cells.f6 for el in entry
     ] == ["NaN" if np.isnan(el) else el for entry in expected_cells.f6 for el in entry]
 
-    for index in chain(range(3, 6), range(7, 21)):
+    mocked_matlab_future = mock.MagicMock()
+    mocked_engine = mock.MagicMock()
+    mocked_matlab_future.result.return_value = mocked_engine
+    with mock.patch.object(
+        RATapi.wrappers.MatlabWrapper,
+        "loader",
+        mocked_matlab_future,
+    ), mock.patch.object(RATapi.rat_core, "DylibEngine", mock.MagicMock()), mock.patch.object(
+        RATapi.inputs,
+        "get_python_handle",
+        mock.MagicMock(return_value=dummy_function),
+    ), mock.patch.object(
+        RATapi.wrappers.MatlabWrapper,
+        "getHandle",
+        mock.MagicMock(return_value=dummy_function),
+    ), mock.patch.object(RATapi.wrappers.DylibWrapper, "getHandle", mock.MagicMock(return_value=dummy_function)):
+        assert list(actual_cells.f14) == expected_cells.f14
+
+    for index in chain(range(3, 6), range(7, 14), range(15, 21)):
         field = f"f{index}"
-        if index != 14:
-            assert getattr(actual_cells, field) == getattr(expected_cells, field)
-        else:
-            mocked_matlab_future = mock.MagicMock()
-            mocked_engine = mock.MagicMock()
-            mocked_matlab_future.result.return_value = mocked_engine
-            with mock.patch.object(
-                RATapi.wrappers.MatlabWrapper,
-                "loader",
-                mocked_matlab_future,
-            ), mock.patch.object(RATapi.rat_core, "DylibEngine", mock.MagicMock()), mock.patch.object(
-                RATapi.inputs,
-                "get_python_handle",
-                mock.MagicMock(return_value=dummy_function),
-            ), mock.patch.object(
-                RATapi.wrappers.MatlabWrapper,
-                "getHandle",
-                mock.MagicMock(return_value=dummy_function),
-            ), mock.patch.object(
-                RATapi.wrappers.DylibWrapper, "getHandle", mock.MagicMock(return_value=dummy_function)
-            ):
-                assert list(getattr(actual_cells, field)) == getattr(expected_cells, field)
+        assert getattr(actual_cells, field) == getattr(expected_cells, field)
 
 
 def check_controls_equal(actual_controls, expected_controls) -> None:
