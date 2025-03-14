24b479e32788f9867dd49f6c95a3e499b8e4435e
alexhroom <alex.room@btinternet.com>
2025-03-14 15:20:20 +0000

    makes orsopy actually optional

--- RATapi/__init__.py
+++ RATapi/__init__.py
@@ -1,11 +1,16 @@
 """RATapi is a Python package for modelling, fitting and optimising reflectivity problems."""
 
+from contextlib import suppress
+
 import RATapi.examples as examples
 from RATapi import events, models
 from RATapi.classlist import ClassList
 from RATapi.controls import Controls
 from RATapi.project import Project
 from RATapi.run import run
-from RATapi.utils import convert, orso, plotting
+from RATapi.utils import convert, plotting
 
-__all__ = ["examples", "models", "events", "ClassList", "Controls", "Project", "run", "plotting", "convert", "orso"]
+with suppress(ImportError):  # orsopy is an optional dependency
+    from RATapi.utils import orso as orso
+
+__all__ = ["examples", "models", "events", "ClassList", "Controls", "Project", "run", "plotting", "convert"]
